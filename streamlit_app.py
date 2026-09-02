86c4c3e7e1412c7f2e86c922de44558a93f48605
Petchara Pattarakijwanich <petcharap@gmail.com>
2026-09-02 22:48:11 +0700

    Try writing to data file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,24 @@
 import streamlit as st
 
+# Balloons, for shit and giggles
 st.balloons()
 
+# Data file, from which we will read and write to
 fname = "data_file.txt"
+
+# Read the data file and show content
 f = open(fname, "r")
 data = f.readlines()
 f.close()
+st.write(data)
 
+# Append a line to this data file
+f = open(fname, "a")
+f.write("This is a new line")
+f.close()
+
+# Read the data file again, and show content
+f = open(fname, "r")
+data = f.readlines()
+f.close()
 st.write(data)
